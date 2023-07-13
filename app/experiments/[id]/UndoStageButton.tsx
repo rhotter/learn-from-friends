f905a32c41026313b70bb3bf6864c50acf6adc72
Raffi Hotter <rhotter@users.noreply.github.com>
2023-07-13 12:29:20 -0400

    Add refresh to undo

--- app/experiments/[id]/UndoStageButton.tsx
+++ app/experiments/[id]/UndoStageButton.tsx
@@ -1,18 +1,19 @@
 "use client";
 import { Stage } from "@prisma/client";
 import { setStage } from "@/utils/setStage";
+import { useRouter } from "next/navigation";
 
 export const UndoStageButton = ({ experimentId }: { experimentId: number }) => {
-  // get router
+  const router = useRouter();
 
   return (
     <div className="mt-2">
       <a
         className="cursor-pointer text-slate-400 hover:text-slate-500  text-xs"
-        onClick={(e) => {
-          setStage(experimentId, Stage.SUBMISSIONS);
-          // reset the page
-          window.location.reload();
+        onClick={async (e) => {
+          e.preventDefault();
+          const stage = await setStage(experimentId, Stage.SUBMISSIONS);
+          router.refresh();
         }}
       >
         Undo topic finalization
