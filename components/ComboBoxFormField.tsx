a01cc6db5b6b65690db38a413017e21b5d2148e4
Raffi Hotter <rhotter@users.noreply.github.com>
2023-07-14 22:42:34 -0400

    Make select more mobile friendly

--- components/ComboBoxFormField.tsx
+++ components/ComboBoxFormField.tsx
@@ -1,17 +1,8 @@
 // submit topics
 "use client";
-import { Check, ChevronsUpDown } from "lucide-react";
 import { FieldPath, UseFormReturn } from "react-hook-form";
 import * as z from "zod";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "@/components/ui/command";
+
 import {
   FormControl,
   FormDescription,
@@ -20,13 +11,17 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+
 import { useState } from "react";
 import { Person } from "@prisma/client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export const ComboBoxFormField = ({
   form,
@@ -41,8 +36,6 @@ export const ComboBoxFormField = ({
   label: string;
   description?: string;
 }) => {
-  const [open, setOpen] = useState(false);
-
   return (
     <FormField
       control={form.control}
@@ -50,53 +43,25 @@ export const ComboBoxFormField = ({
       render={({ field }) => (
         <FormItem className="flex flex-col">
           <FormLabel>{label}</FormLabel>
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <FormControl>
-                <Button
-                  variant="outline"
-                  role="combobox"
-                  className={cn(
-                    "w-full justify-between py-2",
-                    !field.value && "text-muted-foreground"
-                  )}
-                >
-                  {field.value
-                    ? items.find((item) => item.value === field.value)?.label
-                    : "Select..."}
-                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-              </FormControl>
-            </PopoverTrigger>
-            <PopoverContent className="w-full p-0">
-              <Command>
-                <CommandInput placeholder="Search..." />
-                <CommandEmpty>No options found.</CommandEmpty>
-                <CommandGroup className="max-w-sm">
-                  {items.map((item) => (
-                    <CommandItem
-                      key={item.value}
-                      value={item.label}
-                      onSelect={() => {
-                        form.setValue(formFieldName, item.value);
-                        setOpen(false);
-                      }}
-                    >
-                      <Check
-                        className={cn(
-                          "mr-2 h-4 w-4",
-                          item.value === field.value
-                            ? "opacity-100"
-                            : "opacity-0"
-                        )}
-                      />
-                      {item.label}
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </Command>
-            </PopoverContent>
-          </Popover>
+          <Select
+            onValueChange={(value) => field.onChange(Number(value))}
+            defaultValue={field.value}
+          >
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue>
+                  {items.find((item) => item.value === field.value)?.label}
+                </SelectValue>
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {items.map((item) => (
+                <SelectItem key={item.value} value={item.value}>
+                  {item.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
           <FormDescription>{description}</FormDescription>
           <FormMessage />
         </FormItem>
