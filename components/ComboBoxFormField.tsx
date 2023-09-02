08ffbaaf0af2225b6535195685607a0ef638cd60
Raffi Hotter <rhotter@users.noreply.github.com>
2023-09-02 18:06:37 -0400

    Change selector

--- components/ComboBoxFormField.tsx
+++ components/ComboBoxFormField.tsx
@@ -22,7 +22,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
 export const ComboBoxFormField = ({
   form,
   items,
@@ -43,25 +42,19 @@ export const ComboBoxFormField = ({
       render={({ field }) => (
         <FormItem className="flex flex-col">
           <FormLabel>{label}</FormLabel>
-          <Select
-            onValueChange={(value) => field.onChange(Number(value))}
-            defaultValue={field.value}
+
+          <select
+            onChange={(e) => field.onChange(Number(e.target.value))}
+            defaultValue=""
+            className="block w-full px-3 py-2 mt-1 text-gray-800 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
           >
-            <FormControl>
-              <SelectTrigger>
-                <SelectValue>
-                  {items.find((item) => item.value === field.value)?.label}
-                </SelectValue>
-              </SelectTrigger>
-            </FormControl>
-            <SelectContent>
-              {items.map((item) => (
-                <SelectItem key={item.value} value={item.value}>
-                  {item.label}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+            <option value="" disabled></option>
+            {items.map((item) => (
+              <option key={item.value} value={item.value}>
+                {item.label}
+              </option>
+            ))}
+          </select>
           <FormDescription>{description}</FormDescription>
           <FormMessage />
         </FormItem>
