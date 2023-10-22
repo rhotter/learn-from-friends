7489ee65c2b414868cb0a8ffb357c00e867689f2
Raffi Hotter <raphael.hotter@gmail.com>
2023-10-21 20:48:55 -0400

    Better date display

--- app/AllEvents.tsx
+++ app/AllEvents.tsx
@@ -1,6 +1,7 @@
 import Link from "@/components/Link";
 import { NewEvent } from "@/components/NewEvent";
 import { prisma } from "@/utils/prisma";
+import { formatDistanceToNow } from "date-fns";
 
 export const AllEvents = async () => {
   const events = await prisma.event.findMany({
@@ -21,6 +22,7 @@ export const AllEvents = async () => {
     </div>
   );
 };
+
 const Event: React.FC<{
   id: number;
   name: string;
@@ -29,7 +31,25 @@ const Event: React.FC<{
   <Link href={`/event/${id}`} className="hover:no-underline">
     <div className="border p-4 border-secondary-background rounded-lg hover:bg-primary-foreground cursor-pointer">
       <h3 className="mb-2 text-foreground">{name}</h3>
-      <p className="text-secondary-foreground">{date.toDateString()}</p>
+      <p className="text-gray-500">{formatDate(date)}</p>
     </div>
   </Link>
 );
+
+const formatDate = (date: Date): string => {
+  const now = new Date();
+  if (date.toDateString() === now.toDateString()) {
+    return "Today";
+  }
+
+  const yesterday = new Date(now);
+  yesterday.setDate(now.getDate() - 1);
+
+  if (date.toDateString() === yesterday.toDateString()) {
+    return "Yesterday";
+  }
+
+  let formattedDate = formatDistanceToNow(date) + " ago";
+  formattedDate = formattedDate.replace("about ", "");
+  return formattedDate;
+};
