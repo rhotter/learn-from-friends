4cb44c78a8e1a76132a9ec07c5e3654968f419ba
Santiago Aranguri <santiago.aranguri@gmail.com>
2024-03-02 13:53:44 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
   return (
     <main>
       <div>
-        <h1>Learning Events</h1>
+        <h1>Collaboration Events</h1>
         {/* @ts-ignore async component */}
         <AllEvents />
       </div>
